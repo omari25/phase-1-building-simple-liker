2b4687c3affbe2338e0a3debc4ee359a44fe03c5
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-26 10:50:03 +0300

    Done.

--- main.js
+++ main.js
@@ -4,29 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-const heart = document.querySelectorAll(".like-glyph");
-const error = document.getElementById('#modal');
 
-for (const one of heart) {
-  one.addEventListener("click", () => {
-    mimicServerCall("http://mimicServer.example.com")
-    .then(() => {
-      if (one.textContent === EMPTY_HEART) {
-        one.textContent = FULL_HEART;
-        one.className = "activated-heart";
-      } else{
-        one.textContent = EMPTY_HEART;
-      }
-    })
-    .catch(err => {
-      error.className = "";
-      error.textContent = err;
-      setTimeout(function(){
-         error.className = "hidden", 3000
+
+document.querySelector('#modal').className = 'hidden'
+const likeButtons = document.querySelectorAll('.like-glyph')
+function myLikes() {
+    likeButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+
+            if (event.target.innerHTML == EMPTY_HEART) {
+                mimicServerCall("")
+                  .then(function(resp) {
+                      event.target.innerHTML = FULL_HEART
+                      event.target.classList.add('activated-heart')
+                  })
+                .catch(function(error) {
+                    let modal = document.querySelector('#modal')
+                    modal.classList.remove('hidden')
+                    modal.innerText = "Random server error. Try again."
+                    setTimeout(() => {
+                        modal.classList.add('hidden')
+                    }, 3000);
+                })
+            } else {
+                event.target.innerHTML = EMPTY_HEART
+                event.target.classList.remove('activated-heart')
+            }
         })
     })
-  })
 }
+myLikes()
 
 
 
